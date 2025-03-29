e8fce2d3d40772c1ed508fd2b47194b19562b77f
Jordan Ocokoljic <jordan@nofrills.software>
2025-03-30 02:41:30 +1100

    Initial commit

---
+++ src/main/java/software/nofrills/taffy/cli/Main.java
@@ -0,0 +1,10 @@
+package software.nofrills.taffy.cli;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
