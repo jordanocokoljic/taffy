8adb002b11e41e69bbd9e93d6ead7ef8920b24fa
Jordan Ocokoljic <jordan@nofrills.software>
2025-03-30 14:59:08 +1100

    Add support for global recipes

--- src/main/java/software/nofrills/taffy/cli/Main.java
+++ src/main/java/software/nofrills/taffy/cli/Main.java
@@ -24,14 +24,56 @@ public class Main {
         }
 
         Map<String, Step[]> recipes = new HashMap<>();
+        loadRecipesFromDirectory(Paths.get(".taffy"), "local", recipes);
+        loadRecipesFromDirectory(getGlobalRecipePath(), "global", recipes);
+
+        String runRecipe = args[0];
+        Step[] matched = recipes.get("local:" + runRecipe);
+        if (matched == null) {
+            matched = recipes.get("global:" + runRecipe);
+        }
+
+        if (matched == null) {
+            System.err.printf("no loaded recipe matched '%s'%n", runRecipe);
+            System.exit(1);
+        }
+
+        Context context = new Context(System.out);
+        for (var step : matched) {
+            step.apply(context);
+        }
+    }
+
+    private static Path getGlobalRecipePath() {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        String configDir;
+        if (os.contains("win")) {
+            configDir = System.getenv("APPDATA");
+        } else {
+            String xdgConfigHome = System.getenv("XDG_CONFIG_HOME");
+            configDir = xdgConfigHome != null ? xdgConfigHome : System.getenv("HOME");
+        }
+
+        if (configDir == null) {
+            return null;
+        }
+
+        return Paths.get(configDir, ".taffy");
+    }
+
+    private static void loadRecipesFromDirectory(Path dir, final String namespace, Map<String, Step[]> recipes) {
+        if (dir == null) {
+            return;
+        }
+
         ObjectMapper mapper = new YAMLMapper();
 
-        Path localCookbook = Paths.get(".taffy");
-        try (var paths = Files.walk(localCookbook)) {
+        try (var paths = Files.walk(dir)) {
             paths.skip(1).forEach(p -> {
                 try {
                     Step[] recipe = mapper.readValue(p.toFile(), new TypeReference<>() {});
-                    recipes.put(FilenameUtils.removeExtension(p.getFileName().toString()), recipe);
+                    recipes.put(namespace + ":" + FilenameUtils.removeExtension(p.getFileName().toString()), recipe);
                 } catch (InvalidTypeIdException e) {
                     System.err.printf("recipe (%s) contained unrecognized step: %s", p, e.getTypeId());
                     System.exit(1);
@@ -40,23 +82,10 @@ public class Main {
                     System.exit(1);
                 }
             });
-        }
-        catch (NoSuchFileException ignored) {}
-        catch (IOException e) {
+        } catch (NoSuchFileException ignored) {
+        } catch (IOException e) {
             System.err.printf("a fatal error occurred when trying to load local recipes: %s", e);
             System.exit(1);
-        }
-
-        String runRecipe = args[0];
-        Step[] matched = recipes.get(runRecipe);
-        if (matched == null) {
-            System.err.printf("no loaded recipe matched %s%n", runRecipe);
-            System.exit(1);
-        }
-
-        Context context = new Context(System.out);
-        for (var step : matched) {
-            step.apply(context);
         }
     }
 }
